10be1a073701e1e095a97505c0c8a921330b25af
6313125Chayakor <chayakorn.jul@student.mahidol.edu>
2021-03-04 10:13:44 +0000

    edit main

--- main.c
+++ main.c
@@ -8,17 +8,23 @@ int main(int argc, char **argv){
   int i,N,j;
   
   Stack s;
-
-
+  s.size=0;
+  s.top=NULL;
  
   for(i=1;i<argc;i++){
    
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
-
+       switch(argv[i][j]){
+        
+       }
+    if(check ) break;
 
      }
-
+    if(s.size>0||check)
+      printf("The parentheses match successfully for %s\n",argv[i]);
+      check=0;
+      pop_all(&s);
 
   }
 
