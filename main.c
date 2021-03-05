c5ec1c61357da9b0dd5aeb9116b1fd818da0f85b
6313125Chayakor <chayakorn.jul@student.mahidol.edu>
2021-03-05 06:49:37 +0000

    main

--- main.c
+++ main.c
@@ -17,16 +17,19 @@ int main(int argc, char **argv){
        /* Use stack to help with the parentheses*/
        switch(argv[i][j])
        {
-        case 1 :
+        case '[' :
+        case '{' : push(&s, argv[i][j]); break;
+        case ']' : if(pop(&s)!='[') check=1; break;
+        case '}' : if(pop(&s)!='{') check=1; break;
        }
        
 
     if(check ) break;
      }
 
-    if(s.size=0||check)
-      printf("The parentheses match successfully for %s\n",argv[i]);
-    if(s.size>0) printf("The parentheses not match");
+    if(s.size>0||check)
+      printf("The parentheses not match for %s\n",argv[i]);
+    else printf("The parentheses match successfully for %s\n",argv[i]);
       check=0;
       pop_all(&s);
     
